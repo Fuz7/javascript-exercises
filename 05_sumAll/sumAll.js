abf47194a2d1d9b804b9a141b121dcc800d7ab00
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2023-06-13 21:42:11 +0800

    Finish javascript 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
 
+    if (min < 0 || max < 0){
+        return "ERROR"
+    } else if (!(Number.isInteger(min) && Number.isInteger(max))){
+        return "ERROR"
+    }
+
+    if (min > max){
+        let temp = max
+        max = min
+        min = temp
+    }
+    for (let i = min;i <= max; i++){
+        sum += i
+    }
+    return sum;
+
+    
 };
 
 // Do not edit below this line
