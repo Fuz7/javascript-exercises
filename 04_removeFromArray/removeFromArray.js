60bcb84536133c3909d06cae476948c60a2bd006
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2023-06-13 20:00:13 +0800

    Finish javascript 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...items) {
+    for (i = 0;i < items.length; i++){
+        let item = items[i]
+        for (j = 0; j < array.length; j++){
+            let arrayItem = array[j]
+            if (arrayItem === item){
+                array.splice(j, 1)
+                break
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
