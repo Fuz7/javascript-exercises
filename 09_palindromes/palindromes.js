f883c73d6a890b67dc4f6c215ee23af2b78cf68d
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2023-06-18 19:40:36 +0800

    Finish javascript 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let punctuation = string.split('!')
+    if (punctuation) string = punctuation.join('').toLowerCase()
+    
 
+    punctuation = string.split(', ')
+    if (punctuation) string = punctuation.join('')
+
+    punctuation = string.split('.')
+    if (punctuation) string = punctuation.join('')
+
+    punctuation = string.split(' ')
+    if (punctuation) string = punctuation.join('')
+
+    let reversed
+    for(let i = string.length - 1;i >= 0; i--){
+
+        if (!(reversed))reversed = string[i]
+        else reversed += string[i]
+    }
+    if (string === reversed) return true
+    return false
 };
 
 // Do not edit below this line
