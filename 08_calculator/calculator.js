3eb2b222b05fa5815787eeac0544bfcc3616ebd8
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2023-06-18 19:01:24 +0800

    Finish javascript 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sums = arr.reduce((item,sum)=> {
+    return sum + item
+  }, 0)
+  return sums
 };
 
-const multiply = function() {
-
+const multiply = function(...a) {
+  let product = 0
+  for (item of a){
+    if (product === 0) product = item
+    else product *= item
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1
+  for (let i = a;i > 0; i--){
+    sum *= i
+  }
+  return sum
 };
 
 // Do not edit below this line
