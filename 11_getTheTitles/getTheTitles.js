081cb9716d14e82113498367c72b9da5f2d1e13a
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2023-06-18 20:44:31 +0800

    Finish javascript 10, 11, 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    arrTitle = arr.map(function(item) {
+        return item.title
+    })
+    return arrTitle
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.sort((a,b) => 
+    {   date = new Date()
+        if (!(a.yearOfDeath)){
+        a.yearofDeath = date.getFullYear()
+    } else if(!(b.yearOfDeath)){
+        b.yearOfDeath = date.getFullYear()
+    }
+        if ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)) return 1
+        return -1
+    })
+    return arr[0]
 };
 
 // Do not edit below this line
